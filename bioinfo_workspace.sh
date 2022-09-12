937c02a8834d6cec542834af3a40d7bbae70b6b9
Lauro Moraes <lauromoraes@ufop.edu.br>
2022-09-12 15:48:00 -0300

    Update bioinfo_workspace.sh

--- bioinfo_workspace.sh
+++ bioinfo_workspace.sh
@@ -2,17 +2,15 @@
 
 # ======= COPY FILES FROM REPO =======
 cd ~
-mkdir -p "data"
-cd "data"
+mkdir -p "~/nup715-apostila/data"
+cd "~/nup715-apostila/data"
 wget -O - https://codeload.github.com/bioinfonupeb/nup715/tar.gz/main | tar -xz --strip=2 "nup715-main/data";
-cd ..
 
-mkdir -p "backup"
-cd "backup"
+mkdir -p "~/nup715-apostila/backup"
+cd "~/nup715-apostila/backup"
 wget -O - https://codeload.github.com/bioinfonupeb/nup715/tar.gz/main | tar -xz --strip=2 "nup715-main/backup";
-cd ..
 
-mkdir -p "useless-folder"
-cd "useless-folder"
+mkdir -p "~/nup715-apostila/useless-folder"
+cd "~/nup715-apostila/useless-folder"
 wget -O - https://codeload.github.com/bioinfonupeb/nup715/tar.gz/main | tar -xz --strip=2 "nup715-main/useless-folder";
 cd ~
