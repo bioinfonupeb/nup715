19b426b93c1962fd7bee8288d1f4374a984e1f45
Lauro Moraes <lauromoraes@ufop.edu.br>
2022-09-12 15:20:57 -0300

    Update bioinfo_workspace.sh

--- bioinfo_workspace.sh
+++ bioinfo_workspace.sh
@@ -4,15 +4,15 @@
 cd ~
 mkdir -p "data"
 cd "data"
-wget -O - https://codeload.github.com/bioinfonupeb/nup715-2021/tar.gz/main | tar -xz --strip=2 "nup715-2021-main/data";
+wget -O - https://codeload.github.com/bioinfonupeb/nup715/tar.gz/main | tar -xz --strip=2 "nup715-main/data";
 cd ..
 
 mkdir -p "backup"
 cd "backup"
-wget -O - https://codeload.github.com/bioinfonupeb/nup715-2021/tar.gz/main | tar -xz --strip=2 "nup715-2021-main/backup";
+wget -O - https://codeload.github.com/bioinfonupeb/nup715/tar.gz/main | tar -xz --strip=2 "nup715-main/backup";
 cd ..
 
 mkdir -p "useless-folder"
 cd "useless-folder"
-wget -O - https://codeload.github.com/bioinfonupeb/nup715-2021/tar.gz/main | tar -xz --strip=2 "nup715-2021-main/useless-folder";
+wget -O - https://codeload.github.com/bioinfonupeb/nup715/tar.gz/main | tar -xz --strip=2 "nup715-main/useless-folder";
 cd ~
