a05182a7c7499b595474fcc3d4d9f1ccd2c88492
Lauro Moraes <lauromoraes@ufop.edu.br>
2022-09-12 17:47:59 -0300

    Update prepare-workspace.sh

--- prepare-workspace.sh
+++ prepare-workspace.sh
@@ -3,14 +3,14 @@
 BASE="~/nup715-apostila";
 
 cd ~;
-mkdir -p "{$BASE}/data";
-cd "{$BASE}/data";
+mkdir -p "${BASE}/data";
+cd "${BASE}/data";
 wget -O - https://codeload.github.com/bioinfonupeb/nup715/tar.gz/main | tar -xz --strip=2 "nup715-main/data";
 
-mkdir -p "{$BASE}/backup";
-cd "{$BASE}/backup";
+mkdir -p "${BASE}/backup";
+cd "${BASE}/backup";
 wget -O - https://codeload.github.com/bioinfonupeb/nup715/tar.gz/main | tar -xz --strip=2 "nup715-main/backup";
 
-mkdir -p "{$BASE}/useless-folder";
-cd "{$BASE}/useless-folder";
+mkdir -p "${BASE}/useless-folder";
+cd "${BASE}/useless-folder";
 wget -O - https://codeload.github.com/bioinfonupeb/nup715/tar.gz/main | tar -xz --strip=2 "nup715-main/useless-folder";
